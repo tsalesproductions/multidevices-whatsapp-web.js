e26a331e88d921d75657278fdf555eba0430f83d
tsalesproductions <thiago_salests@hotmail.com>
2022-02-28 19:55:19 -0300

    base funcional

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,32 +1,58 @@
 
 const qrcode = require('qrcode-terminal');
+const qrcodeP = require('qrcode')
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const fs = require('fs');
 const { data } = require("../config/clients.json");
 
+let usersStatus = [];
+
 module.exports = class clients{
+    getClients(){
+        return usersStatus;
+    }
     init(){
-        let list = [];
         for(let c of data){
+            let index = null;
+
             const cl = new Client({
                 authStrategy: new LocalAuth({ clientId: "client-"+c.nome })
             });
 
-            cl.on('qr', qr => {
-                qrcode.generate(qr, {small: true});
+            index = usersStatus.findIndex(s => s.nome === usersStatus[c.nome]);
+
+            if(index <= 0){
+                usersStatus.push({
+                    name: c.nome,
+                    status: null
+                })
+
+                index = usersStatus.findIndex(s => s.nome === usersStatus[c.nome]);
+            }
+
+            cl.on('qr', async (qr) => {
+                usersStatus[index].status = 0
+                usersStatus[index].qr = await qrcodeP.toDataURL(qr);
+                usersStatus[index].msg = "Aguardando leitura do QR";
             });
 
             cl.on('authenticated', (session) => {    
-                console.log("autenticado")
+                usersStatus[index].status = 1;
+                usersStatus[index].qr = null;
+                usersStatus[index].msg = "Autenticado";
+                usersStatus[index].cl = cl
                 // Save the session object however you prefer.
                 // Convert it to json, save it to a file, store it in a database...
             });
 
             cl.on('ready', () => {
-                console.log('Client is ready!');
+                usersStatus[index].status = 1;
+                usersStatus[index].qr = null;
+                usersStatus[index].msg = "Ready";
             });
 
             cl.initialize();
+            
         }
     }
 }
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
+const fs = require('fs');
+const express = require("express");
+const app = express();
+
 const clients = new(require("./controllers/clients"))
 
 clients.init();
-// const client = new Client({
-//     authStrategy: new LocalAuth()
-// });
 
-// const SESSION_FILE_PATH = './session.json';
+const sendMessage = (number = null, text = null, cl) => {
+    number = number.replace('@c.us', '');
+    number = `${number}@c.us`
+    const message = text || `Olá, eu sou um BOT`;
+    cl.sendMessage(number, message);
+}
 
-// client.on('qr', qr => {
-//     qrcode.generate(qr, {small: true});
-// });
+app.get("/", (req, res) => {
+    res.json({message: 'ok'});
+});
 
-// client.on('authenticated', (session) => {    
-//     // Save the session object however you prefer.
-//     // Convert it to json, save it to a file, store it in a database...
-// });
+app.get("/send/:name", (req, res) => {
+    const { name } = req.params;
+    const {phone, text} = req.query;
 
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-// });
+    if(!name || !phone || !text) return res.json({status: "client or number or text not found"});
 
-// client.initialize();
+    console.log("ok")
+
+    let data = new(require("./controllers/clients"))().getClients()
+
+    let find = data.find( f=> f.name === name);
+    if(!find || !find.cl) return res.json({status: "aguardando autenticação"});
+
+    sendMessage(phone, text, find.cl)
+    res.json({status: "Mensagem enviada com sucesso!"})
+    
+})
+
+app.get("/auth/:name", (req, res) => {
+    const { name } = req.params;
+
+    let data = new(require("./controllers/clients"))().getClients()
+
+    if(!name) return res.json({status: "client not found"});
+
+    let find = data.find( f=> f.name === name);
+
+    if(!find) return res.send(`Carregando... <script>setTimeout(() => {window.location.reload;}, 1000);</script>`);
+    
+    data = find;
+    
+    if(data.status === 0 || data.status === null){
+        if(!data.qr){
+            res.send(`Verificando se está autenticado, aguarde... <script>setTimeout(()=> {window.location.reload()}, 3000);</script>`);
+            
+        }else{
+            res.send(`<img src="${data.qr}" width="300"> <script>   setTimeout(()=> {window.location.reload()}, 2000);</script>`);
+        }
+    }else{
+        res.json({status: data.status, msg: data.msg});
+    }
+    
+});
+ 
+app.listen(3000, () => {
+    console.log(`Running`);
+})
